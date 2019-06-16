24190ff4df9bf6999dc44aed5a44075c0c45f241
siddharthdixit1 <siddharthdixit1@gmail.com>
2019-06-16 23:34:55 +0530

    import necessary libraries for machine learning project

---
+++ machine_learning_steps.py
@@ -0,0 +1,8 @@
+# import libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# if using Jupyter notebook then include following statement as well
+%matplotlib inline
