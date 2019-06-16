89d07ca328d3289191ebecf12fcc01148058c92e
siddharthdixit1 <siddharthdixit1@gmail.com>
2019-06-16 23:38:06 +0530

    added data preprocessing steps

--- machine_learning_steps.py
+++ machine_learning_steps.py
@@ -6,3 +6,17 @@ import seaborn as sns
 
 # if using Jupyter notebook then include following statement as well
 %matplotlib inline
+
+# load the dataset
+data = pd.read_csv(filepath)
+
+# explore data
+data.info()
+
+# summary statistics
+data.describe()
+
+# common data pre-processing steps
+# missing data imputation
+# encoding categorical variables
+# standardizing the data
